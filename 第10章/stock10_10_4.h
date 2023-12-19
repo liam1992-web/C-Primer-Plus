1d930eae63619e934648c042ce58ce7adb8aa750
Liam.xu <61104738+liam1992-web@users.noreply.github.com>
2023-12-19 21:57:58 +0800

    Add files via upload

---
+++ 第10章/stock10_10_4.h
@@ -0,0 +1,25 @@
+#ifndef STOCK10_H_
+#define STOCK10_H_
+
+#include <string>
+
+class Stock
+{
+private:
+    std::string company;
+    long shares;
+    double share_val;
+    double total_val;
+    void set_tot(){total_val = shares * share_val;}
+
+public:
+    Stock();        //默认构造函数
+    Stock(const std::string & co, long n = 0, double pr = 0.0);     //构造函数
+    ~Stock();       //析构函数
+    void buy(long num, double price);
+    void sell(long num, double price);
+    void update(double price);
+    void show();
+};
+
+#endif
---
+++ 第10章/usestock2_10_6.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include "stock10_10_4.h"
+
+int main()
+{
+    {
+    using std::cout;
+    cout << "Using constructors to create new objects\n";
+    Stock stock1("NanoSmart", 20, 12.50);       //创建类对象 使用构造函数初始化 隐式
+    stock1.show();      //使用类方法
+    Stock stock2 = Stock("Boffo Objects", 2, 2.0);      //显式初始化
+    /*显式初始化的两种实现方式，一种与隐式方式相同，一种通过调用构造函数创建一个临时对象，然后将临时对象复制到stock2中，并丢弃它（使用析构函数）*/
+    stock2.show();
+
+    cout << "Assigning stock1 to stock2:\n";
+    stock2 = stock1;            //对象赋值
+    cout << "Listing stock1 and stock2:\n";
+    stock1.show();
+    stock2.show();
+
+    cout << "Using a constructor to reset an object\n";     
+    stock1 = Stock("Nifty Foods", 10, 50.0);        
+    //对stock1进行赋值，不是初始化，实现方式是调用构造函数创建一个新的、临时的对象，将其内容赋值给stock1，然后再调用析构函数删除临时对象
+    cout << "Revised stock1:\n";        //所以打打印此句之前会有析构函数的打印信息
+    stock1.show();
+    cout << "Done\n";
+    }       //增加大括号，使得程序结束时调用析构函数在return之前，如果没有，将会在return后执行，方框限定了范围
+            //最后将调用析构函数，对象存储在栈中，stock1先入栈，后打印，stock2后入栈，先打印
+    return 0;
+}
