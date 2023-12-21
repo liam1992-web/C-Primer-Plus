f43f23f0df91361056be8a0c19dbd881d51cf24d
Liam.xu <61104738+liam1992-web@users.noreply.github.com>
2023-12-21 10:07:57 +0800

    Add files via upload

---
+++ 第2章/myfirst_2_1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+//使用cin和cout进行输入和输出程序必须包含文件iostream
+int main(){
+	using namespace std;
+	cout << "Come up and C++ me some time.";
+	cout << endl;
+	cout << "You won't regret it!" << endl;
+	cin.get();
+	return 0;
+}
+//endl将导致屏幕光标移动到下一行开头
