8d38950c94f2dd6c31b4d3bb01c573a4442463d0
Liam.xu <61104738+liam1992-web@users.noreply.github.com>
2023-12-21 10:19:10 +0800

    Add files via upload

---
+++ 第11章/mytime0_11_2.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include "mytime0_11_1.h"
+
+Time::Time()
+{
+    hours = minutes = 0;
+}
+
+Time::Time(int h, int m)
+{
+    hours = h;
+    minutes = m;
+}
+
+void Time::AddMin(int m)
+{
+    minutes += m;
+    hours += minutes / 60;
+    minutes %= 60;
+}
+
+void Time::AddHr(int h)
+{
+    hours += h;
+}
+
+void Time::Reset(int h, int m)
+{
+    hours = h;
+    minutes = m;
+}
+
+//返回类型Time意味着程序在删除sum之前构造它的拷贝，调用函数将得到该拷贝
+Time Time::Sum(const Time & t) const
+{
+    Time sum;
+    sum.minutes = minutes + t.minutes;
+    sum.hours = hours + t.hours + sum.minutes / 60;
+    sum.minutes %= 60;
+    return sum;
+}
+
+void Time::Show() const
+{
+    std::cout << hours << " hours, " << minutes << " minutes";
+}
+
