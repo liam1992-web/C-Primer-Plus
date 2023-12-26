51ffcc534c5f73b25c87d6ea9438f994f386a0d8
Liam.xu <61104738+liam1992-web@users.noreply.github.com>
2023-12-26 16:14:26 +0800

    Update hexoct_3_3.cpp

--- 第3章/hexoct_3_3.cpp
+++ 第3章/hexoct_3_3.cpp
@@ -1,12 +1,14 @@
+// shoes hex and octal literals
+
 #include <iostream>
 int main()
 {
     using namespace std;
-    int chest = 42;
-    int waist = 0x42;
-    int inseam = 042;
+    int chest = 42;        // decimal integer literal
+    int waist = 0x42;      // hexadecimal integer literal
+    int inseam = 042;      // octal integer literal
 
-    cout << "Monsieur cuts a striking figure!\n";
+    cout << "Monsieur cuts a striking figure!\n";    // cout以十进制格式显示整数，而不管这些整数在程序中是如何书写的
     cout << "chest = " << chest << " (42 in decimal)\n";
     cout << "waist = " << waist << " (0x42 in hex)\n";
     cout << "inseam = " << inseam << " (042 in octal)\n";
