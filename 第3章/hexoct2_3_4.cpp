ee77c1e715a8db22cd8995203d638294d5050a45
Liam.xu <61104738+liam1992-web@users.noreply.github.com>
2023-12-26 16:19:18 +0800

    Update hexoct2_3_4.cpp

--- 第3章/hexoct2_3_4.cpp
+++ 第3章/hexoct2_3_4.cpp
@@ -1,3 +1,5 @@
+// display values in hex and octal
+
 #include <iostream>
 int main()
 {
@@ -5,7 +7,7 @@ int main()
     int chest = 42;
     int waist = 42;
     int inseam = 42;
-
+    // 可使用控制符dec、hex、oct，分别用于指示cout以十进制、十六进制和八进制格式显示整数
     cout << "Monsieur cuts a striking figure!\n";
     cout << "chest = " << chest << " (decimal for 42)" << endl;
     cout << hex;
