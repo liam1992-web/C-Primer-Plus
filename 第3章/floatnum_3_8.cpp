a7454fc9fa48e98f5c8c0fd3c2348d9713b82668
Liam.xu <61104738+liam1992-web@users.noreply.github.com>
2023-12-27 11:25:20 +0800

    Update floatnum_3_8.cpp

--- 第3章/floatnum_3_8.cpp
+++ 第3章/floatnum_3_8.cpp
@@ -1,11 +1,13 @@
+// floating-point types
+
 #include <iostream>
 int main()
 {
     using namespace std;
     cout.setf(ios_base::fixed,ios_base::floatfield);//这种调用迫使输出使用定点表示法，以便更好地了解精度，它防止程序把较大的值切换为E表示法，并使程序显示到小数点后6位
                                                         //参数ios_base::fixed和ios_base::floatfield时通过包含iostream来提供的常量
-    float tub = 10.0 / 3.0;
-    double mint = 10.0 / 3.0;
+    float tub = 10.0 / 3.0;    // good to about 6 place
+    double mint = 10.0 / 3.0;    // good to about 15 places
     const float million = 1.0e6;
 
     cout << "tub = " << tub;
