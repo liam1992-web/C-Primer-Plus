ca93ddf43bdcc89b9fd81b2180c6fd10d595b574
Liam.xu <61104738+liam1992-web@users.noreply.github.com>
2023-12-29 14:37:16 +0800

    Update instr3_4_5.cpp

--- 第4章/instr3_4_5.cpp
+++ 第4章/instr3_4_5.cpp
@@ -1,3 +1,4 @@
+// reading more than one word with get()&get()
 #include <iostream>
 int main()
 {
@@ -7,7 +8,7 @@ int main()
     char dessert[ArSize];
 
     cout << "Enter your name:\n";
-    cin.get(name,ArSize).get();               
+    cin.get(name,ArSize).get();    //后面的get()将换行符消耗掉，get()没有参数时读取一个字符
     cout << "Enter your favorite dessert:\n";
     cin.get(dessert,ArSize).get();
     cout << "I have some delicious " << dessert;
