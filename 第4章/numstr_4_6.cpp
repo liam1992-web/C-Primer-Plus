eef146895019fac7af571b3989ff195f26af3673
Liam.xu <61104738+liam1992-web@users.noreply.github.com>
2023-12-29 14:39:58 +0800

    Update numstr_4_6.cpp

--- 第4章/numstr_4_6.cpp
+++ 第4章/numstr_4_6.cpp
@@ -1,11 +1,13 @@
+// following number input with line input
+
 #include <iostream>
 int main()
 {
     using namespace std;
     cout << "What year was your house built?\n";
     int year;
-    cin >> year;  //(cin >> year).get(); (cin >> year).get(ch);
-    cin.get(); //cin.get(ch);
+    cin >> year;  //(cin >> year).get(); (cin >> year).get(ch); 
+    cin.get(); //cin.get(ch); cin读取年份，将回车键生成的换行符留在输入队列中，在下一个cin输入时，看到换行符认为是空行
     cout << "What is its street address?\n";
     char address[80];
     cin.getline(address,80);
