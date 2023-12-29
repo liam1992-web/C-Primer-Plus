4c783071713b5071ef18cb7dacf55b18e44d3deb
Liam.xu <61104738+liam1992-web@users.noreply.github.com>
2023-12-29 11:46:46 +0800

    Update instr2_4_4.cpp

--- 第4章/instr2_4_4.cpp
+++ 第4章/instr2_4_4.cpp
@@ -1,3 +1,5 @@
+// reading more than one string
+
 #include <iostream>
 int main()
 {
@@ -7,6 +9,8 @@ int main()
     char dessert[ArSize];
 
     cout << "Enter your name:\n";
+    //cin >> name;       cin使用空白（空格、制表符、换行符）来确定字符串的结束位置，这意味着cin在获取字符数组时只读取一个单词
+    // 且输入的字符串长度可能会比目标数组长
     cin.getline(name,ArSize);               //面向行的输入
     cout << "Enter your favorite dessert:\n";
     cin.getline(dessert,ArSize);
