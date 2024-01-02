2e74e85f2ab506a9ed7149e73580b2d67d5e496f
Liam.xu <61104738+liam1992-web@users.noreply.github.com>
2024-01-02 10:08:20 +0800

    Update forstr2_5_9.cpp

--- 第5章/forstr2_5_9.cpp
+++ 第5章/forstr2_5_9.cpp
@@ -1,3 +1,5 @@
+// reversing an array
+
 #include <iostream>
 #include <string>
 
@@ -8,9 +10,9 @@ int main()
     string word;
     cin >> word;
     
-    char temp;
-    int i,j;
-    for(j = 0,i = word.size() - 1; j < i; --i,++j)
+    char temp;    // temp在循环外声明，若将其放在循环体中会在每轮循环中都被分配和释放，这样允许速度会慢一些，另外在内部时循环结束temp将消失
+    int i,j;     //comma is a separator here, not an operator，在此处声明i,j 在for循环控制的初始化部分无法声明两条语句
+    for(j = 0,i = word.size() - 1; j < i; --i,++j)    //使用逗号运算符，将两条或更多语句放到C++句法只能放一条语句的地方
     {
         temp = word[i];
         word[i] = word[j];
