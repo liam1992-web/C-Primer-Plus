c7a79fb3f38c573a9e53deb8f527fa7f4c267c89
liam1992-web <1065395970@qq.com>
2024-01-03 10:59:01 +0800

    update and_6_5.cpp

--- 第6章/and_6_5.cpp
+++ 第6章/and_6_5.cpp
@@ -1,36 +1,47 @@
+// using the logical AND operator
+
 #include <iostream>
+
 const int ArSize = 6;
+
 int main()
 {
     using namespace std;
+    
     float naaq[ArSize];
+    
     cout << "Enter the NAAQs (New Age Awareness Quotients) "
         << "of\nyour neighbors. Program terminates "
         << "when you make\n" << ArSize << " entries "
         << "or enter a negative value.\n";
     
     int i = 0;
+    
     float temp;
+    
     cout << "First value: ";
     cin >> temp;
-    while(i < ArSize && temp >= 0)
+    while(i < ArSize && temp >= 0)	// 2 quitting criteria
     {
         naaq[i] = temp;
         ++i;
-        if(i < ArSize)
+        if(i < ArSize)		// room left in the array
         {
             cout << "Next value: ";
-            cin >> temp;
+            cin >> temp;	// so get next value
         }
     }
+
     if(i == 0)
         cout << "No data--bye\n";
     else
     {
         cout << "Enter your NAQQ: ";
-        float you;
+        
+	float you;
         cin >> you;
-        int count = 0;
+        
+	int count = 0;
         for(int j = 0; j < i; j++)
             if(naaq[j] > you)
                 ++count;
