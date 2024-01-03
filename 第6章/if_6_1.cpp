ba926fb441d6e9f71a60f8a944f1ecd9fb5f6800
liam1992-web <1065395970@qq.com>
2024-01-03 09:42:16 +0800

    update if_6_1.cpp

--- 第6章/if_6_1.cpp
+++ 第6章/if_6_1.cpp
@@ -1,18 +1,20 @@
+// using the if statement
+
 #include <iostream>
 
 int main()
 {
-    using std::cin;
+    using std::cin;     // using declarations
     using std::cout;
     char ch;
     int spaces = 0;
     int total = 0;
     cin.get(ch);
-    while(ch != '.')
+    while(ch != '.')	// quit at end of sentence
     {
-        if(ch == ' ')
+        if(ch == ' ')	// check if ch is a space
             ++spaces;
-        ++total;
+        ++total;	// done every time
         cin.get(ch);
     }
     cout << spaces << " spaces, " << total;
