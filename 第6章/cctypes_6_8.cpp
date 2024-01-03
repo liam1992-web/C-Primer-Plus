c3d284dadcad5c6062933b31b08123531b0fb216
liam1992-web <1065395970@qq.com>
2024-01-03 14:52:46 +0800

    update cctypes_6_8.cpp

--- 第6章/cctypes_6_8.cpp
+++ 第6章/cctypes_6_8.cpp
@@ -1,5 +1,7 @@
+// use the ctype.h library
+
 #include <iostream>
-#include <cctype>
+#include <cctype>	// prototypes for character functions
 
 int main()
 {
@@ -13,20 +15,20 @@ int main()
     int punct = 0;
     int others = 0;
 
-    cin.get(ch);
+    cin.get(ch);	// get first character
     while(ch != '@')
     {
-        if(isalpha(ch))
+        if(isalpha(ch))	// is it an alphabetic character? 如果使用'a''z'来进行判断，默认字母的字符编码是连续的，其他字符编码不在这个范围内，这个假设对于ASCII是成立的，但通常并非如此
             chars++;
-        else if(isspace(ch))
+        else if(isspace(ch))	//is it an whitespace character? 空白字符计数包括换行符
             whitespace++;
-        else if(isdigit(ch))
+        else if(isdigit(ch))	// is it a digit?
             digits++;
-        else if(ispunct(ch))
+        else if(ispunct(ch))	// is it punctuation?
             punct++;
         else 
             others++;
-        cin.get(ch);
+        cin.get(ch);		// get next character
     }
     cout << chars << " letters, "
         << whitespace << " whitespace, "
