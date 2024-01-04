5fb4378670f6ff6dd550fe40beaa6924b04d6daf
liam1992-web <1065395970@qq.com>
2024-01-04 15:40:47 +0800

    update arrfun2_7_6.cpp

--- 第7章/arrfun2_7_6.cpp
+++ 第7章/arrfun2_7_6.cpp
@@ -1,16 +1,20 @@
+// functions with an array argument
+
 #include <iostream>
 
 const int ArSize = 8;
 
 int sum_arr(int arr[], int n);
+// use std:: instead of using directive
 
 int main()
 {
     //using namespace std;
 
     int cookies[ArSize] = {1,2,4,8,16,32,64,128};
-    
+   // some systems require preceding int with static to enable array initialization 
     std::cout << cookies << " = array address, ";
+    // some system require a type cast: unsigned (cookies)
     std::cout << sizeof cookies << " = sizeof cookies\n";       //输出的时数组的长度
 
     int sum = sum_arr(cookies, ArSize);
@@ -25,10 +29,12 @@ int main()
     return 0;      
 }
 
+// return the sum of an integer array
 int sum_arr(int arr[], int n)
 {
     int total = 0;
     std::cout << arr << " = arr, ";
+    // some system require a type cast: unsigned (arr)
     std::cout << sizeof arr << " = sizeof arr\n";       //输出的是指针变量的长度
 //作为函数参数传入的数组名，在函数内部作为指针变量，这是必须显式传递数组长度而不能在函数内使用sizeof计算数组长度的原因，指针本身并没有指出数组的长度
     for(int i = 0; i < n; i++)
