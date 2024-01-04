c13224a8a39f83ede082704c4fe8e123312de32d
liam1992-web <1065395970@qq.com>
2024-01-04 10:08:50 +0800

    update calling_7_1.cpp

--- 第7章/calling_7_1.cpp
+++ 第7章/calling_7_1.cpp
@@ -1,16 +1,19 @@
+// defining, prototyping, and calling a function
+
 #include <iostream>
 
-void simple();
+void simple();	// function prototype
 
 int main()
 {
     using namespace std;
     cout << "main() will call the simple() function:\n";
-    simple();
+    simple();	// function call
+    cout << "main() is finished with the simple() function.\n";
     return 0;
 }
 
-void simple()
+void simple()	// function definition
 {
     using namespace std;
     cout << "I'm but a simple function.\n";
