6a654486c3f5f04315ad493dde7e6ba49ffa166d
liam1992-web <1065395970@qq.com>
2024-01-04 16:36:40 +0800

    update arrfun4_7_8.cpp

--- 第7章/arrfun4_7_8.cpp
+++ 第7章/arrfun4_7_8.cpp
@@ -1,3 +1,4 @@
+// functions with an array range
 //使用数组区间的函数
 #include <iostream>
 
@@ -10,12 +11,13 @@ int main()
     using namespace std;
 
     int cookies[ArSize] = {1,2,4,8,16,32,64,128};
+    // some systems require preceding int with static to enable array initialization
 
     int sum = sum_arr(cookies, cookies + ArSize);
     cout << "Total cookies eaten: " << sum << endl;
-    sum = sum_arr(cookies, cookies + 3);
+    sum = sum_arr(cookies, cookies + 3);	// first 3 elements
     cout << "First three eaters ate " << sum << " cookies.\n";
-    sum = sum_arr(cookies + 4, cookies + 8);
+    sum = sum_arr(cookies + 4, cookies + 8);	// last 4 elements
     cout << "Last four eaters ate " << sum << " cookies.\n";
     
     return 0;
@@ -23,7 +25,7 @@ int main()
 
 
 
-
+// return the sum of an integer array
 int sum_arr(const int* begin, const int* end)   //end指向数组最后元素的后一个元素
 {
     const int* pt;
