c039b61c7ba06bfb3ddff235098730d3d91ae403
liam1992-web <1065395970@qq.com>
2024-01-04 14:36:54 +0800

    update twoarg_7_3.cpp

--- 第7章/twoarg_7_3.cpp
+++ 第7章/twoarg_7_3.cpp
@@ -1,6 +1,10 @@
+// a function with 2 arguments
+
 #include <iostream>
+
 using namespace std;
 void n_chars(char,int);
+
 int main()
 {
     int times;
@@ -8,7 +12,7 @@ int main()
 
     cout << "Enter a character: ";
     cin >> ch;
-    while(ch != 'q')
+    while(ch != 'q')	// q to quit
     {
         cout << "Enter a integer: ";
         cin >> times;
@@ -24,8 +28,8 @@ int main()
     return 0;
 }
 
-void n_chars(char c,int n)
+void n_chars(char c,int n)	// display c n times
 {
-    while(n-- > 0)
+    while(n-- > 0)	// continue until n reaches 0
         cout << c;
 }
