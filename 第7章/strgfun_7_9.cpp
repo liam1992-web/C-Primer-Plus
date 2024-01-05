0fb5c3d68f97fcb6c6c8945f27ae974fbb407ad0
liam1992-web <1065395970@qq.com>
2024-01-05 09:50:50 +0800

    update strgfun_7_9.cpp

--- 第7章/strgfun_7_9.cpp
+++ 第7章/strgfun_7_9.cpp
@@ -1,3 +1,5 @@
+// function with a string argument
+
 #include <iostream>
 
 unsigned int c_in_str(const char* str, char ch);
@@ -7,7 +9,9 @@ int main()
     using namespace std;
 
     char mmm[15] = "minimum";
-    char* wail = "ululate";
+    // string in an array
+    // some systems require preceding char with static to enable array initialization
+    char* wail = "ululate";	// wail pointer to string
 
     unsigned int ms = c_in_str(mmm, 'm');
     unsigned int us = c_in_str(wail, 'u');
@@ -18,17 +22,18 @@ int main()
     return 0;
 }
 
+// this function counts the number of ch characters in the string str
 unsigned int c_in_str(const char* str, char ch)
 {
     unsigned int count = 0;
 
-    while(*str)
+    while(*str)  // quit when *str is '\0'
     {
 
-        if(*str == ch)  //处理字符串，结尾是'\0'
+        if(*str == ch)  
             count++;
 
-        str++;          //指针后移
+        str++;          //指针后移 move pointer to next char
     }
 
     return count;
