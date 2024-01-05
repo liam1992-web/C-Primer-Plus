7c24851246be62d89d59d686039b9ec42bc962af
liam1992-web <1065395970@qq.com>
2024-01-05 13:53:25 +0800

    update arrobj_7_15.cpp

--- 第7章/arrobj_7_15.cpp
+++ 第7章/arrobj_7_15.cpp
@@ -1,11 +1,15 @@
+// functions with array objects(C++11)
 #include <iostream>
 #include <array>
 #include <string>
 
+// constant data
 const int Seasons = 4;
 const std::array<std::string, Seasons> Snames = {"Spring", "Summer", "Fall", "Winter"};
 
+// function to modify array object
 void fill(std::array<double, Seasons>* pa);
+// function that uses array object without modifying it
 void show(std::array<double, Seasons> da);
 
 int main()
@@ -26,7 +30,7 @@ void fill(std::array<double, Seasons>* pa)
     {
         cout << "Enter " << Snames[i] << " expenses: ";
 
-        cin >> (*pa)[i];
+        cin >> (*pa)[i]; // pa是一个指向array<double,4>对象的指针，因此*pa是这种对象，而(*pa)[i]是该对象的一个元素，由于运算符优先级影响，其中括号必不可少。
     }
 }
 
