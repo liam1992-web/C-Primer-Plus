e40c57022ac043dfcce7de8435c178caa1b14eb1
Liam.xu <61104738+liam1992-web@users.noreply.github.com>
2024-01-06 22:14:18 +0800

    Update strquote_8_7.cpp

--- 第8章/strquote_8_7.cpp
+++ 第8章/strquote_8_7.cpp
@@ -1,3 +1,5 @@
+// different designs
+
 #include <iostream>
 #include <string>
 
@@ -14,10 +16,12 @@ int main()
     string result;
 
     cout << "Enter a string: ";
-    getline(cin, input);
+    getline(cin, input);    // 从输入流中读取一行字符串，直到遇到换行符
     copy = input;
     cout << "Your string as entered: " << input << endl;
     result = version1(input, "***");                        //第二个参数，将char指针赋值给string引用
+    // 在version1()中，temp是一个新的string对象，只在函数中有效，函数执行完毕后，它将不再存在，因此返回指向temp的引用不可行，因此该函数返回类型为string
+    // 这意味着temp的内容被复制到一个临时存储单元中，然后再main()中，该存储单元的内容被复制到一个名为result的string中
     cout << "Your string as enhanced: " << result << endl;
     cout << "Your original string: " << input << endl;
 
@@ -27,7 +31,7 @@ int main()
 
     cout << "Resetting original string.\n";
     input = copy;
-    result = version3(input, "***");            //当指向到此处时程序会崩溃
+    result = version3(input, "***");            //当指向到此处时程序会崩溃，程序试图引用已经释放的内存
     cout << "Your string as enhanced: " << result << endl;
     cout << "Your original string: " << input << endl;
 
@@ -42,15 +46,15 @@ string version1(const string & s1, const string & s2)
     return temp;
 }
 
-const string & version2(string & s1, const string & s2)
+const string & version2(string & s1, const string & s2)    // has side effect
 {
     s1 = s2 + s1 + s2;
-    return s1;
+    return s1;    // safe to return reference passed to function
 }
 
-const string & version3(string & s1, const string & s2)
+const string & version3(string & s1, const string & s2)    // bad design
 {
     string temp;
-    temp = s2 + s1 + s2;
-    return temp;    //return的是临时对象，函数调用时引用导致崩溃    
+    temp = s2 + s1 + s2;    // unsafe to return reference to local variable
+    return temp;    //return的是临时对象，函数调用时引用导致崩溃,程序试图引用已经释放的内存
 }
