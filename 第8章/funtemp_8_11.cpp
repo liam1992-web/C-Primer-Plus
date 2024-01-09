22f4c727ab92e8b266f40e82210cadb11fbfaa7b
liam1992-web <1065395970@qq.com>
2024-01-09 16:55:13 +0800

    update funtemp_8_11.cpp

--- 第8章/funtemp_8_11.cpp
+++ 第8章/funtemp_8_11.cpp
@@ -1,5 +1,8 @@
+// using a function template
+
 #include <iostream>
 
+// function template prototype
 template <typename T>
 void Swap(T & a, T & b);
 
@@ -11,23 +14,24 @@ int main()
     int j = 20;
     cout << "i, j = " << i << ", " << j << ".\n";
     cout << "Using compiler-generated int swapper:\n";
-    Swap(i, j);
+    Swap(i, j);		// generates void Swap(int &, int &)
     cout << "Now i, j = " << i << ", " << j << ".\n";
 
     double x = 24.5;
     double y = 81.7;
-    cout << "i, j = " << i << ", " << j << ".\n";
+    cout << "x, y = " << x << ", " << y << ".\n";
     cout << "Using compiler-generated double swapper:\n";
-    Swap(i, j);
-    cout << "Now i, j = " << i << ", " << j << ".\n";
+    Swap(x, y);		// generates void Swap(double &, double &)
+    cout << "Now x, y = " << x << ", " << y << ".\n";
 
     return 0;
 }
 
-template <typename T>
+// function template definition
+template <typename T>	// or class T
 void Swap(T & a, T & b)
 {
-    T temp;
+    T temp;	// temp a variable of type T 
     temp = a;
     a = b;
     b = temp;
