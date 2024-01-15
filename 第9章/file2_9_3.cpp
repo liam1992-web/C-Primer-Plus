2829b39d1e6ab3ea05fb3a0acadd8b1c07691d5d
Liam.xu <61104738+liam1992-web@users.noreply.github.com>
2024-01-15 10:53:13 +0800

    Update file2_9_3.cpp

--- 第9章/file2_9_3.cpp
+++ 第9章/file2_9_3.cpp
@@ -1,17 +1,21 @@
+// contains functions called in file1.cpp
+
 #include <iostream>
 #include <cmath>
-#include "coordin_9_1.h"
+#include "coordin_9_1.h"    // sturcutre templates, function prototypes
 
+// convert rectangular to polar coordinates
 polar rect_to_polar(rect xypos)
 {
     using namespace std;
-    polar answer;
+    polar answer;        
 
     answer.distance = sqrt(xypos.x * xypos.x + xypos.y * xypos.y);
     answer.angle = atan2(xypos.y, xypos.x);
-    return answer;
+    return answer;            // returns a polar structure
 }
 
+// show polar coordinates, converting angle to degrees
 void show_polar(polar dapos)
 {
     using namespace std;
