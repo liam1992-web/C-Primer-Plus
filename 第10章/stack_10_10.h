c10364503b5d35f9f7eaba4ef1ee0c9082f98b54
liam1992-web <1065395970@qq.com>
2024-01-17 11:00:06 +0800

    update stack_10_10.h

--- 第10章/stack_10_10.h
+++ 第10章/stack_10_10.h
@@ -1,3 +1,5 @@
+// class definition for the stack ADT
+
 #ifndef STACK_H_
 #define STACK_H_
 
@@ -6,15 +8,16 @@ typedef unsigned long Item;
 class Stack
 {
     private:
-        enum {MAX = 10};
-        Item items[MAX];
-        int top;
+        enum {MAX = 10};	// constant specific to class
+        Item items[MAX];	// holds stack items
+        int top;		// index for top stack item
     public:
         Stack();
         bool isempty() const;
         bool isfull() const;
-        bool push(const Item & item);
-        bool pop(Item & item);
+        bool push(const Item & item);	// push() returns false if stack already is full, true otherwise
+        bool pop(Item & item);		// pop() returns false if stack already is empty, true otherwise
+					// pop top into item
 };
 
 #endif
