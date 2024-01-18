59a0c453f1c06f4b58ddc0e3f532043c70c83f9d
liam1992-web <1065395970@qq.com>
2024-01-18 20:36:59 +0800

    update stone1_11_21.cpp

--- 第11章/stone1_11_21.cpp
+++ 第11章/stone1_11_21.cpp
@@ -1,9 +1,12 @@
+// user defined conversion functions
+
 #include <iostream>
 #include "stonewt1_11_19.h"
 
 int main()
 {
     using std::cout;
+
     Stonewt poppins(9, 2.8);
     double p_wt = poppins;      //隐式转换
     cout << "Convert to double => ";
