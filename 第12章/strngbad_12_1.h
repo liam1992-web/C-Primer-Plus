f728d46a0a44546737f8be51a28dd6f3b3d16a8b
liam1992-web <1065395970@qq.com>
2024-01-24 14:51:18 +0800

    update strngbad_12_1.h

--- 第12章/strngbad_12_1.h
+++ 第12章/strngbad_12_1.h
@@ -7,9 +7,9 @@
 class StringBad
 {
     private:
-        char * str;	// pointer to string
+        char * str;	// pointer to string ,使用指针意味着没有为字符串本身分配存储空间，使用构造函数中new来为字符串分配空间,这避免了在类声明中预先定义字符串的长度
         int len;	// length of string
-        static int num_strings;		// number of objects
+        static int num_strings;		// number of objects  使用静态类成员，类的所有对象共享这一个静态成员
     public:
         StringBad(const char * s);	// constructor
         StringBad();			// default constructor
