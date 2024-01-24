c960edf69876d6d5f7c661eb00b31ba90ac3fb66
liam1992-web <1065395970@qq.com>
2024-01-24 14:51:39 +0800

    update vegnews_12_3.cpp

--- 第12章/vegnews_12_3.cpp
+++ 第12章/vegnews_12_3.cpp
@@ -1,3 +1,4 @@
+// using new and delete with classes
 #include <iostream>
 using std::cout;
 #include "strngbad_12_1.h"
@@ -8,25 +9,32 @@ void callme2(StringBad);
 int main()
 {
     using std::endl;
+
     {
         cout << "Starting an inner block.\n";
+
         StringBad headline1("Celery Staiks at Midnight");
         StringBad headline2("Lettuce Prey");
         StringBad sports("Spinach Leaves Bowl for Dollars");
+	
         cout << "headline1: " << headline1 << endl;
         cout << "headline2: " << headline2 << endl;
         cout << "sports: " << sports << endl;
+
         callme1(headline1);
         cout << "headline1: " << headline1 << endl;
+	
         callme2(headline2);     
         /*将headline2作为函数参数来传递，因为值传递，会先调用类的构造函数来创建对象的副本，在函数结束时由于弹栈而被销毁，导致析构函数被调用*/
         cout << "headline2: " << headline2 << endl;
+
         cout << "Initialize one object to another:\n";
         StringBad sailor = sports;
         //等效于StringBad sailor = StringBad(sports);
         //等号右边的构造函数不同于类声明中的构造函数，它的原型是StringBad(const StringBad &)
         //当使用一个对象来初始化另一个对象时，编译器会自动生成上述构造函数，成为复制构造函数，因为它创建对象的一个副本
-        /*C++自动为类重载赋值运算符
+        /*   C++自动为类重载赋值运算符
+	
         原型 Class_name & Class_name::operator=(const Class_name &);
         它接受并返回一个指向类对象的引用
         当将已有的对象赋值给另一个对象时，将使用重载的赋值运算符
